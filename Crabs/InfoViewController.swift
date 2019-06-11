d9994d07ef3f3c08d43d4ed16298d28c1286c234
Kennon Ward <kennon.mward@gmail.com>
2019-06-11 11:40:25 -0400

    finished most if project but still need to build the game screen

--- Crabs/InfoViewController.swift
+++ Crabs/InfoViewController.swift
@@ -9,22 +9,26 @@
 import UIKit
 
 class InfoViewController: UIViewController {
+    
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 160, height: 80))
 
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(label)
+        label.text = "Crabs"
+        label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 48)
+        label.center = view.center
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Crabs/SettingsViewController.swift
+++ Crabs/SettingsViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    var rollIsOn: Bool = true
+    var zoomIsOn: Bool = true
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +21,11 @@ class SettingsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func toggleRoll(_ sender: UISwitch) {
     }
-    */
+    
+    
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+    }
 
 }
